a16f525e4d95064374caa6402639ffd5cfa8897d
Leon Kunert <info@leonkunert.de>
2014-05-31 06:31:22 +0200

    Added the first tests

---
+++ src/literals_test.rs
@@ -0,0 +1,4 @@
+#[test]
+fn int_test () {
+    assert_eq!(10u32, 10u64);
+}
---
+++ src/test.rs
@@ -0,0 +1,14 @@
+#[test]
+fn boolean_test () {
+    assert_eq!(true, true);
+}
+
+#[test]
+fn int_test () {
+    assert_eq!(10, 10u32);
+}
+
+#[test]
+fn float_test () {
+    assert_eq!(10.0, 10f64);
+}
