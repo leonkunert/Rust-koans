76de908d686a2b70a6744d3ac57240c9b2f4ef7b
Leon Kunert <info@leonkunert.de>
2014-06-05 21:01:01 +0200

    Added struct test

--- src/test.rs
+++ src/test.rs
@@ -1,14 +1,25 @@
 #[test]
 fn boolean_test () {
-    assert_eq!(true, true);
+    assert_eq!(true, false);
 }
 
 #[test]
 fn int_test () {
-    assert_eq!(10, 10u32);
+    assert_eq!(10, 10.00);
 }
 
 #[test]
 fn float_test () {
     assert_eq!(10.0, 10f64);
+}
+
+struct point3d {
+    x: f64,
+    y: f64,
+    z: f64
+}
+
+#[test]
+fn point3d_test () {
+    let point = point3d {x: 3.333, y:41.12, z:32322.343};
 }
